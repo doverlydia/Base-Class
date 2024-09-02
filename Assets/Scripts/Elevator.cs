f1ae0fb91516e54ac4a635b54a4ce5eaa25bcfa6
Lydia Dover <68915121+doverlydia@users.noreply.github.com>
2024-09-02 11:18:47 +0300

    Update Elevator.cs
    lydia's comments

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Elevator
 {
+    // ususally for private variables there is a naming convention - underscore, so this would be: int _currentFloor = 0; this is done to diffrentiate class varibales from method variables
     int currentFloor = 0;
 
     public int ButtonPressed(int floorNumber)
@@ -25,6 +26,13 @@ public class Elevator
 
     public void SabathMode(bool isSabath)
     {
+        /*  
+        bracets in if are nessecerray only if there is more than one line below the if, so you could do the following:
+        
+        if (isSabath)
+            Debug.Log("stops at every floor for 3 minutes");
+        */
+        
         if (isSabath)
         {
             Debug.Log("stops at every floor for 3 minutes");
@@ -41,6 +49,19 @@ public class Elevator
         {
             Debug.Log("somthing is blocking the door, keeping doors open");
         }
+
+        //you could use early exit here like the following:
+        /*
+            if (isDoorNotObstructed)
+            {
+                Debug.Log("doors closing");
+                // this part will exit the function, preventing running the code below the return. this is called "Early Exit"
+                return;
+            }
+
+            // this part would happen only if we didn't enter the if body, since if we did - we would return from the function and would never reach below the return statement.
+            Debug.Log("somthing is blocking the door, keeping doors open");
+        */
     }
 }
 
