23041a926aecd72fbb21e5569745585b96ec6117
Lydia Dover <68915121+doverlydia@users.noreply.github.com>
2024-09-02 11:29:21 +0300

    Update ElevetorOporator.cs
    lydia's comments

--- Assets/Scripts/ElevetorOporator.cs
+++ Assets/Scripts/ElevetorOporator.cs
@@ -15,7 +15,7 @@ public class ElevetorOporator : MonoBehaviour
 
         int targetFloor = elevator.ButtonPressed(floorNumber);
 
-
+        // this is huge moolitic code that is better extracted into methods
         if (!isSabath && isDoorNotObstructed)
         {
             if (targetFloor > currentFloor)
@@ -25,6 +25,9 @@ public class ElevetorOporator : MonoBehaviour
                 Debug.Log($"u are on floor {currentFloor} and u pressed {targetFloor}, going up {currentFloor + floorNumber} floors");
                 currentFloor = targetFloor;
                 Debug.Log($"u are on floor {currentFloor}");
+                // insted:
+                // GoUp();
+                // return; - use early exit to prevent elso if else if .....
             }
             else if (targetFloor < currentFloor)
             {
@@ -32,18 +35,36 @@ public class ElevetorOporator : MonoBehaviour
                 elevator.GoDown(currentFloor - targetFloor);
                 currentFloor = targetFloor;
                 Debug.Log($"u are on {currentFloor} and u pressed {targetFloor}, going down {currentFloor - floorNumber} floors");
+                // insted:
+                // GoDown();
+                // return;
             }
         }
         else if (!isDoorNotObstructed)
         {
             elevator.CloseDoors(false);
             Debug.Log("something is blocking the doors");
+            // return;
         }
         else
         {
             elevator.SabathMode(true);
             Debug.Log("sorry it's sabath u need to stand and stare till u reach your floor");
+            // return;
         }
+    }
 
+    void GoUp()
+    {
+        elevator.CloseDoors(true);
+        elevator.GoUp(targetFloor - currentFloor);
+        currentFloor = targetFloor;
+    }
+
+    void GoDown
+    {
+        elevator.CloseDoors(true);
+        elevator.GoDown(currentFloor - targetFloor);
+        currentFloor = targetFloor;
     }
 }
